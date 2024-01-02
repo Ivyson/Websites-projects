7a4926e397c59c8795e45a9a056f68921f98449c
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-01-02 17:22:26 +0200

    Create script.js

---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,6 @@
+let buttonref = document.querySelectorAll(".Buttonoptions");
+let popmessage = document.querySelector(".popup");
+let newgamebutt = document.getElementById("NewGame");
+let exitgamebutt = document.getElementById("ExitGame");
+let resetbutt = document.getElementById("Restart");
+let message = document.getElementById("Message");
